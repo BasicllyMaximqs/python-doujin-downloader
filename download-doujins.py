33f16b12e315c925deb70c3f735d34ad8e9ee531
mex <65974823+BasicllyMaximqs@users.noreply.github.com>
2021-07-25 11:40:23 +0200

    Update download-doujins.py
    changed to f-strings & shortened declaration names and removed useless conversion

--- download-doujins.py
+++ download-doujins.py
@@ -38,7 +38,7 @@ def download_specific_doujin():
     print("Doujin ID:")
     doujin_id_temp = input()
     
-    print("[!] Attempting to get Doujin (" + doujin_id_temp +")")
+    print(f"[!] Attempting to get Doujin ({doujin_id_temp})")
     
     doujin_try = nhentai.get_doujin(doujin_id_temp)
         
@@ -46,8 +46,8 @@ def download_specific_doujin():
         print("[!] Dupe Found -- Quitting!")
         exit()
             
-    os.mkdir("[" + doujin_id_temp +"] " + doujin_try.titles["english"])
-    shutil.copyfile("index.php", "./[" + doujin_id_temp + "] "+ doujin_try.titles["english"] + "/index.php")
+    os.mkdir(f"[{doujin_id_temp}] {doujin_try.titles['english']}")
+    shutil.copyfile("index.php", f"./[{doujin_id_temp}] {doujin_try.titles['english']}/index.php")
     _custom_length = len(doujin_try.pages)
     
     for x in range(1, _custom_length):
@@ -55,8 +55,8 @@ def download_specific_doujin():
             print("[!] Download Complete")
             exit()
                 
-        print("[!] Downloading Page " + str(x) + " from: " + doujin_try.titles["english"])
-        urllib.request.urlretrieve(doujin_try[x].url, "./[" + doujin_id_temp +"] " + doujin_try.titles["english"] + "/" + str(x) + ".png")
+        print(f"[!] Downloading Page {x} from: {doujin_try.titles['english']}")
+        urllib.request.urlretrieve(doujin_try[x].url, f"./[{doujin_id_temp}] {doujin_try.titles['english']}/{str(x)}.png")
     exit()
 
 def loop_downloader():
@@ -64,7 +64,7 @@ def loop_downloader():
     global _global_limit
     while _startint < _global_limit:
         _startint = _startint + 1
-        print("[!] Waiting 2 Seconds for #" + str(_startint))
+        print(f"[!] Waiting 2 Seconds for #{_startint}")
         time.sleep(2)
         start_download()
 
@@ -77,11 +77,11 @@ def start_download():
     
     print("[!] Checking for dupes..")
     
-    if path.exists("[" + str(_startint) + "] " + _doujin.titles["english"]):
+    if path.exists(f"[{_startint}] {_doujin.titles['english']}"):
         print("[!] Dupe Found -- Skipping!")
         return;
     
-    print("[!] Preparing download:  https://nhentai.net/g/" + str(_startint) + "/")
+    print(f"[!] Preparing download for ID {_startint}")
 
     # loli check
     
@@ -98,37 +98,37 @@ def start_download():
                     is_loli = True
                     break;
                 else:
-                    print("[!] Ignored non-Loli tag (" + _doujin.tags[tag_content].name + ")")
+                    print(f"[!] Ignored non-Loli tag ({_doujin.tags[tag_content].name})")
             else:
-                print("[!] Ignored non-tag object (" + str(_length_loli_check) + ")")
+                print(f"[!] Ignored non-tag object ({str(_length_loli_check)})")
 
     if is_loli:
-        print("[!] Loli Doujin was removed from list: (" + _doujin.titles["english"] + ")")
+        print(f"[!] Loli Doujin was removed from list: ({_doujin.titles['english']})")
     else:
         download_doujin(_startint)
         
 def download_doujin(_id):
-    print("[!] Starting Download: " + str(_id) + " - Wait")
+    print(f"[!] Starting Download of ID {_id} - Preparing")
     
     _doujin_temp = nhentai.get_doujin(_id)
     
-    print("[!] Making Title directory: " + _doujin_temp.titles["english"])
-    os.mkdir("[" + str(_id) + "] " + _doujin_temp.titles["english"])
-    print("[!] Making Index: " + _doujin_temp.titles["english"])
-    shutil.copyfile("index.php", "./[" + str(_id) + "] "+ _doujin_temp.titles["english"] + "/index.php")
+    print(f"[!] Making Title directory: {_doujin_temp.titles['english']}")
+    os.mkdir(f"[{_id}] {_doujin_temp.titles['english']}")
+    print(f"[!] Making Index: {_doujin_temp.titles['english']}")
+    shutil.copyfile("index.php", f"./[{_id}] {_doujin_temp.titles['english']}/index.php")
     
     _doujin_pages_length = len(_doujin_temp.pages)
     
     for x in range(1, _doujin_pages_length):
         if x > _doujin_pages_length:
-            print("[!] Doujin (" + _doujin_temp.titles["english"] + ") was downloaded!")
+            print(f"[!] Doujin {_doujin_temp.titles['english']}) was downloaded!")
             break;
         
-        print("[!] Downloading Page " + str(x) + " from: " + _doujin_temp.titles["english"])
-        urllib.request.urlretrieve(_doujin_temp[x].url, "./" + "[" + str(_id) + "] " +_doujin_temp.titles["english"] + "/" + str(x) + ".png")
+        print(f"[!] Downloading Page {x} from: {_doujin_temp.titles['english']}")
+        urllib.request.urlretrieve(_doujin_temp[x].url, f"./[{_id}] {_doujin_temp.titles['english']}/{x}.png")
         
         
-    print("[!] Doujin Download Done (" + _doujin_temp.titles["english"] + ")")
+    print(f"[!] Downloading of '{_doujin_temp.titles['english']}' Complete!")
     
 print("Python Doujin Downloader - Written by BasicllyMaximqs\n[!] Warning: Root is required.")
 main_menu()
